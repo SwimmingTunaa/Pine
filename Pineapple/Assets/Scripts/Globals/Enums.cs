94346746083f8121a7c0caa1b09b3ab5f4c3b543
Jake <jakelturner@gmail.com>
2019-02-23 22:45:30 +1100

    Added bubble size enum

--- Pineapple/Assets/Scripts/Globals/Enums.cs
+++ Pineapple/Assets/Scripts/Globals/Enums.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enums : MonoBehaviour
+
+public class Enums
 {
     public enum Weight { lite = 5, medium = 10, heavy = 20 }
+    public enum BubbleSize { sm, md, lg }
 }
