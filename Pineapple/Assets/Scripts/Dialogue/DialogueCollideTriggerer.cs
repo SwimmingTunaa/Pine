7f99bb6fb6dd016370c6b002f24d1584953915bf
Jake <jakelturner@gmail.com>
2019-02-23 22:48:04 +1100

    WIP Dialogue stuff

---
+++ Pineapple/Assets/Scripts/Dialogue/DialogueCollideTriggerer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueCollideTriggerer : MonoBehaviour
+{
+    public Collider2D triggerCollider;
+    private DialogueSequence _ds;
+
+    private void Awake()
+    {
+        _ds = this.GetComponent<DialogueSequence>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (triggerCollider.transform == other.transform)
+        {
+            _ds.startDialogue();
+        }
+    }
+}
---
+++ Pineapple/Assets/Scripts/Globals/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void pauseGame()
+    {
+        Time.timeScale = 0; // TODO: Not sure if this is correct. Something about timescale pausing all scripts and everything?
+    }
+
+    public void resumeGame()
+    {
+        Time.timeScale = 1;
+    }
+}
