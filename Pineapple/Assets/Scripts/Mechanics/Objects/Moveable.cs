f7053b81f82f5e55772d1e66815908199358960b
Tuan PC <tuanvu@live.com.au>
2019-02-25 16:02:55 +1100

    Blocked out the first level

--- Pineapple/Assets/Scripts/Mechanics/Objects/Moveable.cs
+++ Pineapple/Assets/Scripts/Mechanics/Objects/Moveable.cs
@@ -2,45 +2,58 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class Moveable : Interactable
 {
-    public Enums.Weight playerMoveSpeedPenalty;
+    public Enums.Weight playerMoveSpeedPenalty = Enums.Weight.lite;
     private bool _parentThisObj = true;
+    private Rigidbody2D _thisRb;
+    private CharacterController2D _char2D;
+    void Start()
+    {
+        _thisRb = GetComponent<Rigidbody2D>();
+        _char2D = GameObject.FindGameObjectWithTag("Player").GetComponent<CharacterController2D>();
+        initialize();
+    }
 
+    void initialize()
+    {
+        _thisRb.bodyType = RigidbodyType2D.Kinematic;
+        _thisRb.constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezePositionY | RigidbodyConstraints2D.FreezeRotation;
+    }
 
     public override void DoAction(GameObject player)
     {
         base.DoAction(player);
-
         PlayerController pController = player.GetComponent<PlayerController>();
         HingeJoint2D hinge =  player.GetComponent<HingeJoint2D>();
-        Rigidbody2D thisRb = this.gameObject.GetComponent<Rigidbody2D>();
+        
+        MoveObject(_parentThisObj, hinge, pController);
+        _parentThisObj = !_parentThisObj;
+    }
 
-        pController.jumpable = !_parentThisObj;
-        player.GetComponent<CharacterController2D>().flippable = !_parentThisObj;
-
-        if(_parentThisObj)
+    public void MoveObject(bool enabled, HingeJoint2D hinge, PlayerController playerController)
+    {
+        hinge.enabled = enabled;
+        playerController.jumpable = !enabled;
+        playerController.GetComponent<CharacterController2D>().flippable = !enabled;
+        if(enabled)
         {
-            hinge.enabled = true;
-            pController.speed -= (float)playerMoveSpeedPenalty;
-            if(thisRb.bodyType != RigidbodyType2D.Dynamic)
-                thisRb.bodyType = RigidbodyType2D.Dynamic;
-            thisRb.constraints = RigidbodyConstraints2D.FreezePositionY;
-            hinge.connectedBody = thisRb;
+            playerController.speed -= (float)playerMoveSpeedPenalty;
+            if(_thisRb.bodyType != RigidbodyType2D.Dynamic)
+                _thisRb.bodyType = RigidbodyType2D.Dynamic;
+            _thisRb.constraints = RigidbodyConstraints2D.FreezePositionY;
+            hinge.connectedBody = _thisRb;
         }
         else
         {
-            hinge.enabled = false;
-            thisRb.constraints = RigidbodyConstraints2D.FreezePositionX;
-            if(thisRb.velocity.y > -0.1f )
+            _thisRb.constraints = RigidbodyConstraints2D.FreezePositionX;
+            if(_thisRb.velocity.y >= 0f )
             {
-                print("hi");
-                thisRb.constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezePositionY;
+                _thisRb.constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezePositionY;
             }
-            pController.speed = pController.startSpeed;
+            playerController.speed = playerController.startSpeed;
             hinge.connectedBody = null;
         }
-
-        _parentThisObj = !_parentThisObj;
     }
 }
--- Pineapple/Assets/Scripts/Player/Interact.cs
+++ Pineapple/Assets/Scripts/Player/Interact.cs
@@ -8,9 +8,20 @@ public class Interact : MonoBehaviour
     public float InteractDistance;
     public InteractButton interactButton;
 
+    private CharacterController2D _char2D;
+
+    void Start()
+    {
+        _char2D = GetComponent<CharacterController2D>();
+    }
     void Update()
     {
-        RayCast();
+        //make player let go of object if they are falling
+        if(!_char2D.m_Grounded && GetComponent<HingeJoint2D>().enabled && GetComponent<HingeJoint2D>().connectedAnchor != null)
+        {
+            interactButton.InteractableObject.DoAction(gameObject);
+        }
+        RayCast();  
     }
     void RayCast()
     {
@@ -18,7 +29,7 @@ public class Interact : MonoBehaviour
         RaycastHit2D hit = Physics2D.Raycast(transform.position + (Vector3.up * 0.5f), Vector2.right * transform.localScale.x, InteractDistance);
         Debug.DrawRay(transform.position + (Vector3.up * 0.5f), Vector2.right * transform.localScale.x * InteractDistance, Color.red);
 
-        if(hit.collider != null && hit.collider.gameObject.layer == LayerMask.NameToLayer("Interactable"))
+        if(hit.collider != null && hit.collider.gameObject.layer == LayerMask.NameToLayer("Interactable") && _char2D.m_Grounded)
         {
             interactButton.InteractableObject = hit.collider.gameObject.GetComponent<Interactable>();
             interactButton.GetComponentInChildren<Text>().text = hit.collider.gameObject.GetComponent<Interactable>().buttonText;
@@ -28,7 +39,6 @@ public class Interact : MonoBehaviour
             interactButton.InteractableObject = null;
             interactButton.GetComponentInChildren<Text>().text = "Jump";
         }
-           
     }
 }
 
