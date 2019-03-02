b87ccce257bae508a1f140f7d318ce774972347b
Tuan PC <tuanvu@live.com.au>
2019-03-02 21:20:25 +1100

    working on the inventory stem and item pick up

---
+++ Pineapple/Assets/Scripts/Globals/Inventory.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public List<ItemObject> inventoryItems = new List<ItemObject>();
+    public List<Sprite> invetorySpaces = new List<Sprite>();
+}
---
+++ Pineapple/Assets/Scripts/Mechanics/Objects/Item.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : Interactable
+{
+    public ItemObject itemObject;
+    public Collider2D otherCollider;
+    public int triggerAmount;
+
+    private Inventory _inventory;
+
+    void Awake()
+    {
+        _inventory = GameObject.FindGameObjectWithTag("GameController") .GetComponent<Inventory>();
+        itemObject.Initialize(this.gameObject);
+    }
+
+    //At the moment the item gets pick up when the player walks over it
+    //Not sure if we want to do pick up with the interact button
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(otherCollider.transform == other.transform && triggerAmount > 0)
+        {
+            DoAction(other.gameObject);
+        }
+    }
+    public override void DoAction(GameObject player)
+    {
+        triggerAmount--;
+        base.DoAction(player);
+        _inventory.inventoryItems.Add(itemObject);
+
+        if(itemObject.pickUpSound)
+            GetComponent<AudioSource>().PlayOneShot(itemObject.pickUpSound);
+        if(itemObject.deathFX)
+            Instantiate(itemObject.deathFX,transform.position,transform.rotation);
+        
+        gameObject.SetActive(false);
+    }
+}
